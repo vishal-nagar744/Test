666b55d88829de65c071728780057cb02735c154
Vishal Nagar <102690538+vishal-nagar744@users.noreply.github.com>
2023-12-04 05:54:50 -0800

    dist

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const { app, BrowserWindow } = require('electron');
+const {autoUpdater, AutoUpdater} = require ('electron-updater')
 
 let mainWindow;
+
+autoUpdater.autoDownload = false;
+autoUpdater.autoInstallOnAppQuit = true;
+
+
 
 function createWindow() {
   mainWindow = new BrowserWindow({
